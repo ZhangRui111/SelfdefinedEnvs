f1f87857bd7298448cc525f38f72ddfa273223b0
ZhangRui111 <zhangruisg111@163.com>
2019-04-12 15:27:47 +0800

    gui maze  initialization
    gui maze  initialization

---
+++ gui_maze/maze.py
@@ -0,0 +1,147 @@
+import numpy as np
+import time
+import tkinter as tk
+
+MAZE_H = 4  # grid height
+MAZE_W = 4  # grid width
+
+
+class Maze(tk.Tk, object):
+    def __init__(self):
+        super(Maze, self).__init__()
+
+        self.unit = 40  # size of one self.unit/pixels.
+        self.action_space = ['u', 'd', 'l', 'r']  # four actions: up, down, left, right.
+        self.n_actions = len(self.action_space)
+
+        self.title('Gui Maze')
+        self.geometry('{0}x{1}'.format(MAZE_H * self.unit, MAZE_H * self.unit))  # windows background.
+        self._build_maze()
+
+    def _build_maze(self):
+        self.canvas = tk.Canvas(self, bg='white', height=MAZE_H * self.unit, width=MAZE_W * self.unit)  # maze.
+
+        # positions
+        self.obstacles_origin = [(2, 1), (1, 2)]  # all obstacles' position.
+        self.exit_origin = [(2, 2)]  # exit's position.
+        self.player_origin = [(0, 0)]  # player's position.
+
+        # create grids.
+        for c in range(0, MAZE_W * self.unit, self.unit):
+            x0, y0, x1, y1 = c, 0, c, MAZE_H * self.unit
+            self.canvas.create_line(x0, y0, x1, y1)
+        for r in range(0, MAZE_H * self.unit, self.unit):
+            x0, y0, x1, y1 = 0, r, MAZE_W * self.unit, r
+            self.canvas.create_line(x0, y0, x1, y1)
+
+        # coordination of the origin point (center of the top left corner unit).
+        self.origin = np.array([self.unit/2, self.unit/2])
+
+        # create obstacle (black rectangle).
+        self.obstacles_coords = []  # hold all obstacles' coordination.
+        for i in range(len(self.obstacles_origin)):
+            obstacle_center = self.origin + np.array(
+                [self.unit * self.obstacles_origin[i][0], self.unit * self.obstacles_origin[i][1]])
+            obstacle = self.canvas.create_rectangle(
+                obstacle_center[0] - 15, obstacle_center[1] - 15,
+                obstacle_center[0] + 15, obstacle_center[1] + 15,
+                fill='black')
+            self.obstacles_coords.append(self.canvas.coords(obstacle))
+
+        # create exit (green oval)
+        exit_center = self.origin + np.array(
+            [self.unit * self.exit_origin[0][0], self.unit * self.exit_origin[0][1]])
+        self.exit = self.canvas.create_oval(
+            exit_center[0] - 15, exit_center[1] - 15,
+            exit_center[0] + 15, exit_center[1] + 15,
+            fill='green')
+
+        # create player (yellow oval)
+        player_center = self.origin + np.array(
+            [self.unit * self.player_origin[0][0], self.unit * self.player_origin[0][1]])
+        self.player = self.canvas.create_oval(
+            player_center[0] - 15, player_center[1] - 15,
+            player_center[0] + 15, player_center[1] + 15,
+            fill='yellow')
+
+        # pack all
+        self.canvas.pack()
+
+    def reset(self):
+        self.update()
+        self.canvas.delete(self.player)
+        # reset the player.
+        player_center = self.origin + np.array(
+            [self.unit * self.player_origin[0][0], self.unit * self.player_origin[0][1]])
+        self.player = self.canvas.create_oval(
+            player_center[0] - 15, player_center[1] - 15,
+            player_center[0] + 15, player_center[1] + 15,
+            fill='yellow')
+        # return observation
+        return self.canvas.coords(self.player)
+
+    def step(self, action):
+        s = self.canvas.coords(self.player)
+        base_action = np.array([0, 0])
+        if action == 0:  # up
+            if s[1] > self.unit:
+                base_action[1] -= self.unit
+        elif action == 1:  # down
+            if s[1] < (MAZE_H - 1) * self.unit:
+                base_action[1] += self.unit
+        elif action == 2:  # left
+            if s[0] > self.unit:
+                base_action[0] -= self.unit
+        elif action == 3:  # right
+            if s[0] < (MAZE_W - 1) * self.unit:
+                base_action[0] += self.unit
+        else:
+            print('illegal action!')
+
+        self.canvas.move(self.player, base_action[0], base_action[1])  # move agent
+
+        s_ = self.canvas.coords(self.player)  # next state
+
+        # reward function
+        if s_ == self.canvas.coords(self.exit):
+            reward = 1
+            done = True
+            info = 'terminal'
+        elif s_ in self.obstacles_coords:
+            reward = -1
+            done = True
+            info = 'terminal'
+        else:
+            reward = 0
+            done = False
+            info = 'running'
+
+        return s_, reward, done, info
+
+    def render(self, slt=None):
+        if slt is not None:
+            time.sleep(slt)
+        self.update()
+
+
+def my_update():
+    for t in range(10):
+        env.reset()
+        while True:
+            env.render()
+            a = np.random.random_integers(4)-1
+            s, r, done, info = env.step(a)
+            if done:
+                print(info)
+                break
+
+
+def main():
+    global env
+    env = Maze()
+    env.after(100, my_update)  # Call function update() once after given time/ms.
+    env.mainloop()  # mainloop() to run the application.
+
+
+if __name__ == '__main__':
+    main()
